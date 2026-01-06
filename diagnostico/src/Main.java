8948cba4ae552b9b5a89bf573d63b1611dd728c5
Fabian Fuentes <20243ds032@utez.edu.mx>
2026-01-06 08:58:40 -0600

    Add files via upload

--- diagnostico/src/Main.java
+++ diagnostico/src/Main.java
@@ -5,15 +5,20 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-
-        int  numero;
-
+        int suma=0;
+        int numero ;
         do {
 
-            System.out.printf("ingresa un numero: ");
-            
+            System.out.println("ingresa un numero: ");
+            numero = sc.nextInt();
 
+            if (numero!=0){
+                System.out.println("numero ingresado: "+numero);
+                suma+=numero;
 
-        }while(numero!=0);
+            }
+
+        } while (numero != 0);
+        System.out.println("el totaL es: "+suma);
     }
 }
